21b743fe59c0e5aa12d242c3fa5128b4dfb51d98
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2025-02-27 10:09:01 +0100

    Add .bin/toggle-dark-mode.sh

---
+++ .bin/toggle-dark-mode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current_mode=$(gsettings get org.gnome.desktop.interface color-scheme)
+
+if [[ "$current_mode" == "'prefer-dark'" ]]; then
+    # gsettings set org.gnome.desktop.interface color-scheme 'default'
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+else
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+fi
